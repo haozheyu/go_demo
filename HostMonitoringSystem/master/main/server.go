cd445896bb82c712037a6ffdd12c39c1259f9c27
1216108213@qq.com <1216108213@qq.com>
2021-02-25 11:33:38 +0800

    update client manager

---
+++ HostMonitoringSystem/master/main/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// master -config ./master.json -xxx 123 -yyy ddd
+	// master -h
+	flag.StringVar(&confFile, "config", "./server.json", "指定server.json")
+	flag.Parse()
+}
+
+// 初始化线程数量
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	var (
+		err error
+	)
+
+	// 初始化命令行参数
+	initArgs()
+
+	// 初始化线程
+	initEnv()
+
+	// 加载配置
+	if err = InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 初始化服务发现模块
+	if err = InitWorkerMgr(); err != nil {
+		goto ERR
+	}
+
+	// 日志管理器
+	if err =InitLogMgr(); err != nil {
+		goto ERR
+	}
+
+	//  任务管理器
+	if err = InitJobMgr(); err != nil {
+		goto ERR
+	}
+
+	// 启动Api HTTP服务
+	if err = InitApiServer(); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+	return
+
+ERR:
+	fmt.Println(err)
+}
